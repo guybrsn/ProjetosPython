1c615ccc9ba9da86f4d502bf7dedf841cdab9287
guybrsn <guibersonpereira@gmail.com>
2026-08-03 11:30:36 -0400

    Tentando resolver o ex 01

---
+++ ProfetosHerança/DsPoligonos/__main__.py
@@ -0,0 +1,15 @@
+from classes import Poligono, Quadrado, Circulo
+
+def main():
+    #quadrado01 = Quadrado(12)
+    #print(f"{quadrado01.area():.1f}")
+    #print(f"{quadrado01.perimetro():.1f}")
+
+
+    circulo01 = Circulo(12)
+    print(circulo01.area())
+    print(circulo01.perimetro())
+
+
+if __name__ == "__main__":
+    main()
--- ProfetosHerança/DsPoligonos/classes.py
+++ ProfetosHerança/DsPoligonos/classes.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 
 class Poligono(ABC):
-    def __init__(self, qtd_lados):
+    def __init__(self, qtd_lados=0):
         self.qtd_lados = qtd_lados
 
 
@@ -17,12 +17,33 @@ class Poligono(ABC):
 
 
 class Quadrado(Poligono):
-    def __init__(self, qtd_lados):
-        super().__init__(qtd_lados)
+    def __init__(self, lado):
+        super().__init__()
+        self.lado = lado
+
+    
+    def area(self):
+        area_quadrado = self.lado * self.lado
+        return f"Area = {area_quadrado}"
+
+    
+    def perimetro(self):
+        perimetro_quadrado = self.lado * 4
+        return f" Perimetro = {perimetro_quadrado}"
+
+
+
+class Circulo(Poligono):
+    def __init__(self, raio):
+        super().__init__(raio)
+        self.Raio:float = raio
 
 
     def area(self):
-        area_quadrado = self.qtd_lados * self.qtd_lados
+        raio_circulo = (self.Raio * self.Raio) * 3.14
+        return f"Area = {raio_circulo}"
 
-        return f"A area do quadrado e {area_quadrado}"
-        
+
+    def perimetro(self):
+        p = 2 * 3.14 * self.Raio
+        return f"Perimetro = {p}"
